b6bf7f89ce63864a2337effd95779f087da0874e
Bellets <bellets96@gmail.com>
2024-09-06 20:24:58 +0200

    fix nome

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Authentic New Era',
+	'name'=>'Authentic Roleplay',
 	'language'=>'it',
 
 	// preloading 'log' component
